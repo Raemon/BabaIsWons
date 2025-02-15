404a947a48246a5979f317258b2cdcb2d8636255
Raemon <raemon777@gmail.com>
2025-02-15 00:51:40 -0800

    update config

--- config.js
+++ config.js
@@ -9,13 +9,13 @@ const config = {
   customLevels: [
     {
       id: '70b15d82-eaa9-11ef-a278-0afffd82ddb9',
-      name: 'Level 1',
-      description: 'The first custom level'
+      name: 'Yet Again Out of Reach',
+      description: 'For People who have never played'
     },
     {
       id: '61dc9d364f80c300151cf338',
-      name: 'Level 2',
-      description: 'Testing'
+      name: 'Hall of Doors',
+      description: 'Tier 2'
     },
     {
       id: "20b5a734-eb3a-11ef-9a40-0afffd82ddb9",
